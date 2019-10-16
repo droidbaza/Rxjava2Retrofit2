6189570b62a63ea2826f04543d37e6648bc500bc
droidbaza <droidbaza@gmail.com>
2019-10-16 22:44:57 +0300

    added butterknife

--- app/src/main/java/com/droidbaza/movies/MainActivity.java
+++ app/src/main/java/com/droidbaza/movies/MainActivity.java
@@ -2,7 +2,6 @@ package com.droidbaza.movies;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.droidbaza.movies.adapter.MoviesAdapter;
 import com.droidbaza.movies.api.ApiService;
@@ -11,6 +10,8 @@ import com.droidbaza.movies.model.Movie;
 import com.droidbaza.movies.model.Page;
 import java.util.ArrayList;
 import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -20,16 +21,17 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.recyclerview)
+    private RecyclerView recyclerView;
+
+
     private List<Movie> movies = new ArrayList<>();
     private MoviesAdapter moviesAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        ButterKnife.bind(this);
 
         moviesAdapter = new MoviesAdapter(MainActivity.this, movies);
         recyclerView.setAdapter(moviesAdapter);
--- app/src/main/java/com/droidbaza/movies/adapter/MoviesAdapter.java
+++ app/src/main/java/com/droidbaza/movies/adapter/MoviesAdapter.java
@@ -6,13 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.recyclerview.widget.RecyclerView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.droidbaza.movies.R;
 import com.droidbaza.movies.model.Movie;
 import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by droidbaza on 16/10/19.
  */
@@ -25,14 +28,18 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         this.context = context;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        private TextView title, overview;
-        private ImageView poster;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            title = (TextView) itemView.findViewById(R.id.title);
-            overview = (TextView) itemView.findViewById(R.id.overview);
-            poster = (ImageView) itemView.findViewById(R.id.poster);
+    class ViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.title)
+        TextView title;
+        @BindView(R.id.overview)
+        TextView overview;
+        @BindView(R.id.poster)
+        ImageView poster;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+
         }
     }
 
@@ -48,6 +55,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
 
     }
 
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
